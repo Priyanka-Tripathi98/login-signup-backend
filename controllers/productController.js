283b6b0d1ba09221a0fe4b46b6ac569e5d30dbd2
Priyanka-Tripathi98 <priyankatripa35@gmail.com>
2026-04-07 11:22:26 +0530

    initialise first commit

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,26 +1,33 @@
 const Products = require("../models/productsmodel")
 
-exports.productData = async(req,res) =>{
-        const{product_name,product_description,product_price,product_category} = req.body
-        console.log(req.body)
-        console.log(req.file);
-    
-    try{
+exports.productData = async (req, res) => {
+    // 1. Check if the file actually exists before proceeding
+    if (!req.file) {
+        return res.status(400).json({ message: "No image uploaded" });
+    }
+
+    const { product_name, product_description, product_price, product_category } = req.body;
+
+    try {
         const productdetails = new Products({
-            product_name: product_name,
-            product_description:  product_description,
-            product_price:  product_price,
-            product_category:  product_category,
+            product_name,
+            product_description,
+            product_price,
+            product_category,
             product_image: req.file.path,
-            publicId: req.filename,
+            publicId: req.file.filename, // Note: Changed from req.filename to req.file.filename
             resourceType: "image",
             mimeType: req.file.mimetype,
-            
-        })
-        await productdetails.save()
-        res.status(201).json({message:"Product added successfully", image: req.file.path})
-    }catch(err){
-        console.log(err);
-        res.status(500).json({message:"Server error"})
+        });
+
+        await productdetails.save();
+        res.status(201).json({ 
+            success: true, 
+            message: "Product added successfully", 
+            image: req.file.path 
+        });
+    } catch (err) {
+        console.error("Database Error:", err);
+        res.status(500).json({ message: "Server error", error: err.message });
     }
-}
+};
